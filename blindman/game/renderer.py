4b235cd76f66b0af2c5556a5e28fdc81c605c7d4
Silvio Mayolo <mercerenies@comcast.net>
2024-01-24 16:49:12 -0500

    Config in renderer

--- blindman/game/renderer.py
+++ blindman/game/renderer.py
@@ -1,19 +1,26 @@
 
+from .config import Configuration
 from blindman.renderer import FrameRenderer
 
 import numpy as np
+import cv2
 
 
 class GameRenderer(FrameRenderer):
 
+    def __init__(self, config: Configuration) -> None:
+        self._config = config
+        self._background_image = cv2.imread(config.background_image)
+
     def total_frames(self) -> int:
-        return 30
+        return 10 * self.fps()  # Ten seconds (until we get a real calculation here)
 
     def fps(self) -> int:
-        return 30
+        return self._config.fps
 
     def frame_size(self) -> tuple[int, int]:
-        return (512, 512)
+        height, width, _ = self._background_image.shape
+        return width, height
 
     def render_frame(self, frame_number: int, canvas: np.ndarray) -> None:
-        canvas[:] = 255
+        canvas[:] = self._background_image
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 
 from blindman.discord import get_avatar
 from blindman.renderer import VideoRenderer
-from blindman.game import GameRenderer
+from blindman.game import GameRenderer, Configuration
+from blindman.lisp import parse_many
+
+test_str = """
+(configuration
+  :background-image "/home/silvio/Pictures/BlindMansRampage/Game1/Floor1/BlindManFloor1_DEBUG.png")
+"""
+
+parsed_file = parse_many(test_str)
 
 renderer = VideoRenderer(
-    frame_renderer=GameRenderer(),
+    frame_renderer=GameRenderer(config=Configuration.from_sexpr(parsed_file[0])),
 )
 
 renderer.render('tmp.mp4')
