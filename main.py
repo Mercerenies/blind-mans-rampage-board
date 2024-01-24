9e5d8cbcf3e6d8346bebf4b3c5511871fc21eea3
Silvio Mayolo <mercerenies@comcast.net>
2024-01-24 15:18:06 -0500

    Testing renderer

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 
 from blindman.discord import get_avatar
+from blindman.renderer import VideoRenderer
+from blindman.game import GameRenderer
 
-x = get_avatar('87351108317491200', size=1024)
-print(len(x))
-with open('tmp', 'wb') as f:
-    f.write(x)
+renderer = VideoRenderer(
+    frame_renderer=GameRenderer(),
+)
 
-'''
-import imageio.v2 as imageio
-
-writer = imageio.get_writer('output1.mp4', fps=30)
-tmp = imageio.imread('imageio:astronaut.png')
-for i in range(1200):
-    print("Frame", i)
-    writer.append_data(tmp)
-
-writer.close()
-'''
+renderer.render('tmp.mp4')
+print("Done.")
