b092dd12dc33fe712faa7a684298553856fa55a9
Silvio Mayolo <mercerenies@comcast.net>
2024-01-25 17:21:17 -0500

    Re-export parse_key_value_list

--- blindman/lisp/__init__.py
+++ blindman/lisp/__init__.py
@@ -3,8 +3,10 @@
 
 from .symbol import Symbol
 from .parser import parse, parse_many, LispParseError
+from .deserialize import parse_key_value_list
 
 __all__ = (
     'Symbol',
     'parse', 'parse_many', 'LispParseError',
+    'parse_key_value_list',
 )
