d69670ec1afc34bcb51e6fa35321972e131105e3
Silvio Mayolo <mercerenies@comcast.net>
2024-01-24 20:29:44 -0500

    example.lisp

---
+++ example.lisp
@@ -0,0 +1,6 @@
+
+(configuration
+  :background-image "/home/silvio/Pictures/BlindMansRampage/Game1/Floor1/BlindManFloor1_DEBUG.png")
+
+(objects
+  (object star "/home/silvio/Documents/star.png" (96 96)))
