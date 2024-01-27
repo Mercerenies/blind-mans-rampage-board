ed911339864a1d9ed36a5553d9a41e3b6b9d0fb9
Silvio Mayolo <mercerenies@comcast.net>
2024-01-26 19:22:57 -0500

    alpha=... in draw()

--- blindman/util/__init__.py
+++ blindman/util/__init__.py
@@ -34,11 +34,17 @@ def pluck(dictionary: dict[_K, _V], keys: Iterable[_K]) -> dict[_K, _V]:
     return {k: dictionary[k] for k in keys if k in dictionary}
 
 
-def draw(destination: np.ndarray, source: np.ndarray, center: tuple[int, int]) -> None:  # Takes (x, y)
+def draw(
+        destination: np.ndarray,
+        source: np.ndarray,
+        center: tuple[int, int],
+        *,
+        alpha: float = 1.0,
+) -> None:
     upperleft_y, upperleft_x = center[0] - source.shape[0] // 2, center[1] - source.shape[1] // 2
     lowerright_y, lowerright_x = upperleft_y + source.shape[0], upperleft_x + source.shape[1]
     destination_patch = destination[upperleft_y:lowerright_y, upperleft_x:lowerright_x, :]
-    source_alpha = source[:, :, (ALPHA_CHANNEL,)] / MAX_BYTE
+    source_alpha = (source[:, :, (ALPHA_CHANNEL,)] / MAX_BYTE) * alpha
     destination_patch = destination_patch * (1 - source_alpha) + source * source_alpha
     destination[upperleft_y:lowerright_y, upperleft_x:lowerright_x, :] = destination_patch.astype(np.uint8)
 
