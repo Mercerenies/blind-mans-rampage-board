3d2316919a5063e5eef25cc0391610f89e83e245
Silvio Mayolo <mercerenies@comcast.net>
2024-01-28 19:48:30 -0500

    update docs

--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 
 This library produces video files replaying a game of Blind Man's
 Rampage.
+
+## Usage
+
+    python3 main.py <input_filename> -o <output_filename>
+
+`input_filename` should be the name of a `.lisp` file, and
+`output_filename` should be the name of the desired result video file.
+All output file formats supported by the `imageio` Python library are
+supported, though this software has been mostly tested with `.mp4`
+files.
+
+If you wish to reference Discord avatars in the input file, you will
+need to register a Discord bot application and set the
+`DISCORD_BOT_TOKEN` environment variable to the application's bot
+token. In that case, it is also recommended (but not required) that
+you have a local Redis server running on `localhost:6379`. If present,
+Redis will be used to cache Discord avatars and avoid unnecessarily
+hitting the Discord servers.
+
+See `example.lisp` for an annotated example input file.
--- example.lisp
+++ example.lisp
@@ -1,49 +1,84 @@
 
+;; This is an example input file for the Blind Man's Rampage video
+;; renderer. Your input file must consist of four S-expressions in the
+;; same order as this file: configuration, spaces, objects, commands.
+;;
+;; All relative file paths in an input file are resolved relative to
+;; the input file itself, NOT to the working directory from which the
+;; program was run.
+
+;; The configuration object describes basic global configuration data
+;; about the resulting video. The configuration object consists of
+;; key-value pairs, where each key is a symbol beginning with a colon.
 (configuration
-  :background-image "/home/silvio/Pictures/BlindMansRampage/Game1/Floor1/BlindManFloor1_DEBUG.png"
-  :fps 60)
+  ;; background-image (required) indicates the image to use as the
+  ;; background for the board. This is drawn behind all objects before
+  ;; each frame. This also determines the size of the canvas.
+  ;;
+  ;; It is recommended that the dimensions of this image be a multiple
+  ;; of 16, and it is recommended that a bit of blank space be left at
+  ;; the bottom, to allow text to be drawn without covering up other
+  ;; content.
+  ;;
+  ;; This must be a file path. It cannot be a Discord user ID.
+  :background-image "Background.png"
+  ;; fps (optional) is the frames per second of the video. Default
+  ;; value is 60.
+  :fps 60
+  ;; start-space (optional) is a symbol representing the name of the
+  ;; starting space. If not provided, defaults to the word "start".
+  :start-space start)
 
+;; This mapping determines where the spaces are located on the board.
+;; Each position in this mapping shall be a pair (x y) of coordinates,
+;; where (0 0) is the top-left corner of the canvas. The start space
+;; named in configuration MUST be present in this mapping.
 (spaces
-  (upper-shop (120 90))
-  (upper-unlucky (334 66))
-  (upper-right-gray-1 (498 61))
-  (upper-right-gray-2 (627 65))
-  (upper-lucky (373 149))
-  (star (511 144))
-  (left-gray-1 (133 229))
-  (left-gray-2 (262 224))
-  (library (512 236))
-  (start (220 327))
-  (center-gray (364 327))
-  (lower-left-lucky (319 410))
-  (lower-center-gray (442 412))
-  (lower-unlucky (547 412))
-  (lower-shop (204 464))
-  (lower-gray-1 (351 476))
-  (lower-gray-2 (485 487))
-  (lower-right-lucky (606 486))
-  (lower-left-gray (217 530))
-  (bowser (484 560)))
+  (shop (120 90))
+  (start (220 327)))
 
+;; The objects initially present in the game room. Each object must be
+;; of the form
+;;
+;; (object name image-path start-space)
+;;
+;; Where the image path can either be a local file path or the string
+;; "discord:" followed by a Discord user ID. In the latter case, the
+;; Discord user's avatar will be retrieved as the image.
 (objects
-  (object star "/home/silvio/Documents/star.png" upper-shop)
-  (object star1 "/home/silvio/Documents/star.png" start)
-  (object mercerenies "discord:87351108317491200" start))
+  (object star "Star.png" shop)
+  (object discord-user "discord:00000000000000000" start))
 
+;; The commands will run in sequence and perform the actual animation
+;; work. All command types are listed below.
 (commands
-  (text "Mercerenies moves right")
-  (move mercerenies center-gray)
-  (wait 60)
-  (text "Merc and the star swapped")
-  (swap star mercerenies)
-  (hide-text)
-  (wait 30)
-  (text "This text has\nmultiple lines")
-  (wait 60)
-  (add sparky "/home/silvio/Documents/sparky.png" library)
-  (wait 30)
-  (text "Sparky moves")
-  (move sparky start)
-  (wait 30)
+  ;; (move player-name space-name)
+  ;;
+  ;; Moves an object to the given space.
+  (move discord-user shop)
+  ;; (swap player1 player2)
+  ;;
+  ;; Swaps two objects' positions.
+  (swap discord-user star)
+  ;; (add name image-path start-space)
+  ;;
+  ;; Adds a new object to the game board. Parameters are interpreted
+  ;; the same as the (object ...) starting form.
+  (add sparky "Sparky.png" shop)
+  ;; (remove name)
+  ;;
+  ;; Removes an object from the game board.
   (remove sparky)
-  (wait 60))
+  ;; (text string)
+  ;;
+  ;; Shows the given text at the bottom of the screen. Text is shown
+  ;; instantaneously and remains until replaced or hidden.
+  (text "Things happened!")
+  ;; (hide-text)
+  ;;
+  ;; Hides any displayed text.
+  (hide-text)
+  ;; (wait frames)
+  ;;
+  ;; Do nothing for the specified amount of time.
+  (wait 30))
